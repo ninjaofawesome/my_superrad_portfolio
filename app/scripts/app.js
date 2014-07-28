78d3845d23a2532613c628e6960a2c9a79086947
Hannah Nordgren <hey@hereshannahs.info>
2014-07-28 18:37:12 -0400

    here we go yo

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mySuperradPortfolioApp
+ * @description
+ * # mySuperradPortfolioApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mySuperradPortfolioApp', []);
