a8078a7b0270e49f17c34a2cc1c6a519ef1e4cab
Hannah Nordgren <hey@hereshannahs.info>
2014-08-03 23:24:03 -0400

    viewheight, ugh

---
+++ app/scripts/resize.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+//     var bodyheight = $(document).height();
+
+//       var tester= $('slider').height(bodyheight);
+//       console.log(tester);
+// });
+
+// // for the window resize
+// $(window).resize(function() {
+//     var bodyheight = $(document).height();
+//     $("#sidebar").height(bodyheight);
+// });
